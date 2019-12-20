70226f9faed67d8b18477ec829442dddac294136
Eugene Vasilenko <gmrnsk@gmail.com>
2019-12-20 12:55:37 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 set -Eeuo pipefail
 set -x
 
-: "${COMMAND?No command to run. Nothing to do.}"
 : "${GITHUB_WORKSPACE?GITHUB_WORKSPACE has to be set. Did you use the actions/checkout action?}"
 pushd ${GITHUB_WORKSPACE}
 
