68482b47b4d3bec793cb8613e97c3c24ebe3cc7a
Eugene Vasilenko <gmrnsk@gmail.com>
2019-12-23 18:53:30 -0800

    remove unused code

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-# : "${GITHUB_WORKSPACE?GITHUB_WORKSPACE has to be set. Did you use the actions/checkout action?}"
-# pushd ${GITHUB_WORKSPACE}
-
-molecule ${INPUT_OPTIONS} ${INPUT_COMMAND} ${INPUT_ARGS}
